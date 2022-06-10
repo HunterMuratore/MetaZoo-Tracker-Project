7068ab762cfee1a6177a08107b06a409d68ac8db
Hunter Muratore <muratoreh@gmail.com>
2022-06-10 14:23:26 -0400

    Replaced MetaZooBox model with MetaZooItem model, added Enums folder

--- Inventory Tracker Project/Models/MetaZooItem.cs
+++ Inventory Tracker Project/Models/MetaZooItem.cs
@@ -1,15 +1,19 @@
-﻿namespace Inventory_Tracker_Project.Models
+﻿using Inventory_Tracker_Project.Enums;
+
+namespace Inventory_Tracker_Project.Models
 {
-    public class MetaZooBox
+    public class MetaZooItem
     {
-        public MetaZooBox(string name, string edition, DateTime releaseDate, int printRun)
+        public MetaZooItem(MetaZooItemType type, string name, string edition, DateTime releaseDate, int printRun)
         {
+            Type = type;
             Name = name;
             Edition = edition;
             ReleaseDate = releaseDate;
             PrintRun = printRun;
         }
 
+        public MetaZooItemType Type { get; }
         public string Name { get; }
         public string Edition { get; }
         public DateTime ReleaseDate { get; }
