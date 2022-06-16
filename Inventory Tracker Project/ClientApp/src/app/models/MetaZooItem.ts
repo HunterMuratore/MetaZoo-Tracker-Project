6ab433d9e1e9afdce4084bb8a5992ee56c99cf83
Hunter Muratore <muratoreh@gmail.com>
2022-06-16 13:09:07 -0400

    Removed namespace from interface model

--- Inventory Tracker Project/ClientApp/src/app/models/MetaZooItem.ts
+++ Inventory Tracker Project/ClientApp/src/app/models/MetaZooItem.ts
@@ -1,12 +1,10 @@
 import { MetaZooItemType } from "../enums/MetaZooItemType";
 
-namespace Inventory_Tracker_Project.ClientApp.src.app.models
-{
-  export interface MetaZooItem {
-    type: MetaZooItemType;
-    name: string;
-    edition: number;
-    releaseDate: Date;
-    printRun: number;
-  }
+export interface MetaZooItem {
+  type: MetaZooItemType;
+  name: string;
+  edition: number;
+  releaseDate: Date;
+  printRun: number;
 }
+
