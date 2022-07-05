4418b1430dc1989d784fc91aae800783f8596830
Hunter Muratore <muratoreh@gmail.com>
2022-07-05 16:28:11 -0400

    Created InventoryRepositoryTest file

---
+++ Inventory Tracker Project Tests/Repositories/InventoryRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using Inventory_Tracker_Project.Enums;
+using Inventory_Tracker_Project.Interfaces;
+using Inventory_Tracker_Project.Models;
+using Inventory_Tracker_Project.Repositories;
+using MongoDB.Driver;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Tracker_Project_Tests.Repositories
+{
+    internal class InventoryRepositoryTest
+    {
+        private readonly MetaZooItem _item = new MetaZooItem(
+            MetaZooItemType.Card,
+            "Test card",
+            "Test edition",
+            DateTime.Now,
+            3);
+
+        private Mock<IMongoCollection<MetaZooItem>> _mockCollection;
+        private Mock<IMongoDatabase> _mockDatabase;
+        private InventoryRepository _repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockCollection = new Mock<IMongoCollection<MetaZooItem>>();
+            _mockDatabase = new Mock<IMongoDatabase>();
+
+            _mockDatabase.Setup(x => x.GetCollection<MetaZooItem>()).Returns();
+
+            _repository = new InventoryRepository(_mockDatabase.Object);
+        }
+    }
+}
