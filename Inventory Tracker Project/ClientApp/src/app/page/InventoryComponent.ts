fbc1d8fe5f43ae4232a6ddc1c481356203e9c149
Brandon Stoeckel <brandonrs522@outlook.com>
2022-07-28 21:13:55 -0400

    Fixed sorting Commented out pagination

--- Inventory Tracker Project/ClientApp/src/app/page/InventoryComponent.ts
+++ Inventory Tracker Project/ClientApp/src/app/page/InventoryComponent.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MetaZooItem } from '../models/MetaZooItem';
 import { InventoryService } from '../services/InventoryService';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-page',
   templateUrl: './InventoryComponent.html',
   styleUrls: ['./InventoryComponent.css']
 })
-export class InventoryComponent implements OnInit {
-  items: MetaZooItem[];
+export class InventoryComponent implements OnInit, AfterViewInit {
+  items: MatTableDataSource<MetaZooItem>;
   columnsToDisplay = ['itemName', 'itemEdition', 'itemReleaseDate', 'itemPrintRun'];
 
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort = new MatSort();
+  // @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private inventoryService: InventoryService) {
-    this.items = [];
+    this.items = new MatTableDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.items.sort = this.sort;
+    // this.items.paginator = this.paginator;
   }
 
   ngOnInit(): void {
     this.inventoryService.getItems().subscribe((items) => {
-      this.items = items;
-      this.items.sort = this.sort;
-      this.items.paginator = this.paginator;
+      this.items = new MatTableDataSource(items);
     });
   }
 }
